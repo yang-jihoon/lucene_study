62a967f43b867c1195692d492f2068508611e613
양지훈 <psalm0105@nate.com>
2012-11-26 01:24:18 +0900

    go it

--- psalm0105/src/IndexFiles.java
+++ psalm0105/src/IndexFiles.java
@@ -1,3 +1,23 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Date;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.NumericField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
 
 public class IndexFiles {
 
@@ -5,8 +25,75 @@ public class IndexFiles {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String docsPath = "E:\\workspace";
+		String indexPath = "E:\\lucene_index";
+		
+	    Date start = new Date();
+		
+		try {
+			Directory dir = FSDirectory.open(new File(indexPath));
+			Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_31);
+			IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_31, analyzer);
+			
+			iwc.setOpenMode(OpenMode.CREATE);
+			IndexWriter writer = new IndexWriter(dir, iwc);
+			
+			final File docDir = new File(docsPath);
+			indexDocs(writer, docDir);
 
+		    writer.close();
+		    
+            Date end = new Date();
+		    System.out.println(end.getTime() - start.getTime() + " total milliseconds");
+
+		} catch (Exception e) {
+			System.out.println(e);  
+		}
 	}
 
+	static void indexDocs(IndexWriter writer, File file)throws IOException {
+    if (file.canRead()) {
+
+      if (file.isDirectory()) {
+        String[] files = file.list();
+        if (files != null) {
+          for (int i = 0; i < files.length; i++) {
+            indexDocs(writer, new File(file, files[i]));
+          }
+        }
+      } else {
+    	if (file.getName().endsWith(".txt") || file.getName().endsWith(".java")) {
+	        FileInputStream fis;
+	        try {
+	          fis = new FileInputStream(file);
+	        } catch (FileNotFoundException fnfe) {
+			  System.out.println(fnfe);  
+	          return;
+	        }
+	
+	        try {
+	          Document doc = new Document();
+	
+	          Field fullPathField = new Field("full_path", file.getPath(), Field.Store.YES, Field.Index.ANALYZED);
+	          doc.add(fullPathField);
+	          
+	          Field fileNameField = new Field("file_name", file.getName(), Field.Store.YES, Field.Index.ANALYZED);
+	          doc.add(fileNameField);
+	
+	          NumericField modifiedField = new NumericField("modified");
+	          modifiedField.setLongValue(file.lastModified());
+	          doc.add(modifiedField);
+	
+	          doc.add(new Field("contents", new BufferedReader(new InputStreamReader(fis, "UTF-8"))));
+
+	          System.out.println("adding " + file);
+	          
+	          writer.addDocument(doc);
+	        } finally {
+	          fis.close();
+	        }
+    	}
+      }
+    }
+  }
 }
--- psalm0105/src/SearchFiles.java
+++ psalm0105/src/SearchFiles.java
@@ -1,3 +1,23 @@
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.BooleanClause.Occur;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
 
 public class SearchFiles {
 
@@ -5,8 +25,60 @@ public class SearchFiles {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String index = "E:\\lucene_index";
+		String field = "contents";
+		String queryString1 = "string";
+		String queryString2 = "int";
+		int hitsPerPage = 10;
+				
+		try {
+			Directory dir = FSDirectory.open(new File(index));
+			IndexReader ir = IndexReader.open(dir);
+			IndexSearcher searcher = new IndexSearcher(ir);
+			Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_31);
+			QueryParser parser = new QueryParser(Version.LUCENE_31, field, analyzer);
+			
+			Query query1 = parser.parse(queryString1);
+			Query query2 = parser.parse(queryString2);
+			
+			BooleanQuery bq = new BooleanQuery();
+			bq.add(query1, Occur.MUST);
+			bq.add(query2, Occur.MUST);
+						
+		    System.out.println("Query String : " + queryString1);
+		    System.out.println("Query String : " + queryString2);
+		    System.out.println("Query : " + bq.toString());
+		    System.out.println("Searching for: " + bq.toString(field));
+		    
+		    TopDocs results = searcher.search(bq, 5 * hitsPerPage);
+		    ScoreDoc[] hits = results.scoreDocs;
 
+		    int numTotalHits = results.totalHits;
+		    System.out.println(numTotalHits + " total matching documents");
+		    
+		    for (int i = 0; i < hits.length; i++) {
+		    	System.out.println("doc="+hits[i].doc+" score="+hits[i].score);
+		    	
+		    	Document doc = searcher.doc(hits[i].doc);
+		        String path = doc.get("full_path");
+		        if (path != null) {
+		          System.out.println((i+1) + ". " + path);
+		        } else {
+		          System.out.println((i+1) + ". " + "No path for this document");
+		        }
+		    }
+
+		    searcher.close();
+		} catch (CorruptIndexException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
